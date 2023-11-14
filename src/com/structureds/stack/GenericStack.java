2abb46f8314e1ab141f09fcb5a2d74b192ec3a14
Narayana87 <lakshmi_litu@yahoo.com>
2023-11-15 00:25:31 +0530

    added more stack methods

--- src/com/structureds/stack/GenericStack.java
+++ src/com/structureds/stack/GenericStack.java
@@ -1,4 +1,40 @@
 package com.structureds.stack;
 
-public class GenericStack {
+import java.util.ArrayList;
+
+public class GenericStack<E> {
+    private ArrayList<E> stackList = new ArrayList<>();
+
+    //pushing element to the stack
+    public void push(E element){
+        stackList.add(element);
+    }
+
+    //poping element from the list
+
+    public void po(){
+        if(!isEmpty()){
+            stackList.remove(stackList.size() - 1);
+        }else {
+            System.out.println("Stack is Empty.");
+        }
+    }
+
+    public boolean isEmpty(){
+        return stackList.isEmpty();
+    }
+    public int size(){
+        return stackList.size();
+    }
+
+    public void display(){
+        if (!isEmpty()){
+            for (E e : stackList
+                 ) {
+                System.out.println(e.toString());
+            }
+        }else {
+            System.out.println("Stack is Empty");
+        }
+    }
 }
